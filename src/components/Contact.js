bc9604cc8f7201cce577c44e03073d55e2900173
krishna das <dask2207@gmail.com>
2024-01-24 18:03:12 +0530

    first test case

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,15 @@
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact</h1>
-            <h2>This is contact us Page</h2>
+        <div className="bg-gray-100 p-2">
+            <h1 className="text-center font-bold text-3xl p-4 m-4">Contact Us</h1>
+            <div className="flex items-center justify-center">
+                <form className="py-10 bg-gray-400 w-80 flex align-center justify-center rounded-lg flex-col  items-center">
+                    <input type="text" className="border border-gray-300 p-2 m-2 outline-none" placeholder="Name" />
+                    <input type="email" className="border border-gray-300 p-2 m-2 outline-none" placeholder="Email" /> 
+                    <input type="text" className="border border-gray-300 p-2 m-2 outline-none" placeholder="Message" />
+                    <button className="bg-blue-600 text-white p-2 m-2 rounded-lg">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
