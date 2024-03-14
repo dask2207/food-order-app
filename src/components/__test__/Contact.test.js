8d3dca786ae1857cdc107bca3855a0fd87ada642
Krishna Das <65500771+dask2207@users.noreply.github.com>
2024-03-14 22:16:20 +0530

    Update Contact.test.js

--- src/components/__test__/Contact.test.js
+++ src/components/__test__/Contact.test.js
@@ -1,6 +1,46 @@
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import Contact from '../Contact';
+import "@testing-library/jest-dom"
 
-test("should load contact us component", ()=> {
-    render(<Contact />);
-})
+
+describe('Contact us page test cases', () => {
+    test("should load contact us component", ()=> {
+        render(<Contact />);
+    
+        const heading = screen.getByRole("heading");
+    
+        // Assertion
+        expect(heading).toBeInTheDocument();
+    })
+    
+    test("should load button inside contact us component", ()=> {
+        render(<Contact />);
+    
+        const button = screen.getByRole("button");
+    
+        // Assertion
+        expect(button).toBeInTheDocument();
+    })
+    
+    test("should load input name inside contact us component", ()=> {
+        render(<Contact />);
+    
+        const inputName = screen.getByPlaceholderText("Name");
+    
+        // Assertion
+        expect(inputName).toBeInTheDocument();
+    })
+    
+    test("should load 2 input boxes on the contact us component", ()=> {
+        render(<Contact />);
+    
+        const inputBoxes = screen.getAllByRole("textbox");
+    
+        console.log(inputBoxes.length);
+    
+        // Assertion
+        expect(inputBoxes.length).toBe(3);
+    })
+});
+
+
