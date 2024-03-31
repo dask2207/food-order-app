295b9ba8c8bc24cdf6e8712b93c31c7796a80ea8
krishna das <dask2207@gmail.com>
2024-03-31 10:54:06 +0530

    fic contact link issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -73,7 +73,7 @@ const Header = () => {
           <Link to="/cart" className="navLink" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Cart ({cartItems.length})</Link>
         </li>
         <li>
-          <a href="/contact" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+          <Link to="/contact" className="navLink" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
         </li>
         <button className="login" onClick={()=>{
                     btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
